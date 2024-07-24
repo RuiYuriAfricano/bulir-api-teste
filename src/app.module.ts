023450d3378b84e1db432db1abe2525e861ed473
RuiYuriAfricano <yuriafricano03@gmail.com>
2024-07-24 16:01:42 +0100

    chore: update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ServicesModule } from './services/services.module';
+import { BookingsModule } from './bookings/bookings.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, ServicesModule],
+  imports: [AuthModule, UsersModule, ServicesModule, BookingsModule],
   providers: [PrismaService],
 })
 export class AppModule { }
