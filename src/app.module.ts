c76285cedc908e4c6e374975631e1dd5500420f8
RuiYuriAfricano <yuriafricano03@gmail.com>
2024-07-24 16:42:27 +0100

    chore: adjust app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ServicesModule } from './services/services.module';
 import { BookingsModule } from './bookings/bookings.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, ServicesModule, BookingsModule],
+  imports: [AuthModule, UsersModule, ServicesModule, BookingsModule, TransactionsModule],
   providers: [PrismaService],
 })
 export class AppModule { }
