9a82dcf7ac910cd98e91e0209fdd3179e0174f55
RuiYuriAfricano <yuriafricano03@gmail.com>
2024-07-24 15:32:08 +0100

    feat: adjust app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-
+import { ServicesModule } from './services/services.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, ServicesModule],
   providers: [PrismaService],
 })
 export class AppModule { }
