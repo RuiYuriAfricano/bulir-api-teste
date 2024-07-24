95c02cdb21ffb36ada89305b9f30489ce8813cfa
RuiYuriAfricano <yuriafricano03@gmail.com>
2024-07-24 12:38:18 +0100

    feat: add jwt autentication

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(private readonly usersService: UsersService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: 'rui1234', // Substitua por uma chave secreta segura
+        });
+    }
+
+    async validate(payload: any) {
+        return await this.usersService.findById(payload.sub);
+    }
+}
